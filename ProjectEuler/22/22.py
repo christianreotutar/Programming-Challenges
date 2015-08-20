36c9cf4c05153aef9c24e11c4c57791681b39879
Christian <christian.reotutar@yahoo.com>
2015-08-20 02:53:42 -0400

    22

--- ProjectEuler/22/22.py
+++ ProjectEuler/22/22.py
@@ -1,44 +1,101 @@
 import fileinput
 import sys
 
-def radixSort(array, maxlen):
-    newarray = array
-    allbuckets = [[]
-    for digit in xrange(maxlen):
-        buckets = [[] for i in xrange(26)]
-        for i in xrange(len(newarray)):
-            name = newarray[i]
-            characters = list(name)
+'''
+stableSort : sorts the array "array" on digit "digit" of every string of
+    "array" and then returns a sorted array which is a stable sorted
+    version of "array"
 
-            letter = ""
-            if (len(characters) - 1< digit):
-                letter = "A"
-            else:
-                letter = characters[digit]
+array : a list of strings
+digit : an zero-index integer to represent digit we sort for
 
-            print(letter)
-            slot = ord(letter) - ord('A')
-            print(slot)
-            buckets[slot].append(name)
+returns : a list of strings
+'''
+def stableSort(array, digit):
+    # bucket[0] = "", bucket[1] = "A", bucket[2] = "B" ...
+    buckets = [[] for i in xrange(27)]
+    newarray = []
+    
+    # go through all strings and add to a bucket
+    for i in xrange(len(array)):
+        name = array[i]
+        characters = list(name)
 
-        newarray = []
-        for i in xrange(len(buckets)):
-            for j in xrange(len(buckets[i])):
-                newarray.append(buckets[i][j])
-            
+        letter = ""
+        if (len(characters) - 1 < digit):
+            letter = chr(ord("A")-1)
+        else:
+            letter = characters[digit]
+
+        slot = ord(letter) - ord('A') + 1
+        buckets[slot].append(name)
+
+    # put all buckets into one newarray
+    for i in xrange(len(buckets)):
+        for j in xrange(len(buckets[i])):
+            newarray.append(buckets[i][j])
+
     return newarray
 
-names = []
-maxlen = 0
+'''
+radixSort : sorts array "array" by going through all digits of all strings
+    in the array in the reverse order and sorting
 
-names = fileinput.input()[0].strip().split(",")
-for i in xrange(len(names)):
-    name = names[i]
-    names[i] = name.replace("\"", "")
+array : list of strings to sort
+maxlen : integer length of the maximum length string in "array"
 
-    if (len(name) > maxlen):
-        maxlen = len(name)
-print(names)
+returns : list of sorted strings in "array"
+'''
+def radixSort(array, maxlen):
+    newarray = array
+    for digit in xrange(maxlen-1, -1, -1):
+        newarray = stableSort(newarray, digit)
+    
+    return newarray
 
-sortedNames = radixSort(names, maxlen)
-print(sortedNames)
+'''
+findNameScore : taking the name "name", finds the value by the sum of all
+    its letters' positions in the alphabet. this value is then multiplied
+    by the names position in the sorted list "numberInList"
+
+name : string name of the person
+numberInList : position in list (not zero-indexed)
+
+returns : integer name score of name "name"
+'''
+def findNameScore(name, numberInList):
+    charList = list(name)
+    score = 0
+    for i in charList:
+        letterScore = ord(i) - ord('A') + 1
+        score += letterScore
+    return score * numberInList
+
+def main():
+    names = []
+    maxlen = 0
+
+    names = fileinput.input()[0].strip().split(",")
+
+    # remove quotation marks in each name, find maxlen
+    for i in xrange(len(names)):
+        name = names[i]
+        names[i] = name.replace("\"", "")
+
+        if (len(name) > maxlen):
+            maxlen = len(name)
+
+    # sort
+    sortedNames = radixSort(names, maxlen)
+
+    # find total name score
+    totalScores = 0
+    for i in xrange(len(sortedNames)):
+        nameScore = findNameScore(sortedNames[i], i + 1)
+        totalScores += nameScore
+
+    print(totalScores)
+        
+
+if __name__ == "__main__":
+    main()
--- ProjectEuler/22/run.sh
+++ ProjectEuler/22/run.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 cat p022_names.txt | python 22.py
+#cat test.txt | python 22.py
